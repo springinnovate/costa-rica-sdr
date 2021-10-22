2e908ef298a42e0c97031b0811e8ae2a6d4f3ebf
Rich Sharp <richpsharp@gmail.com>
2021-10-22 16:25:30 -0700

    make the tablename smaller

--- zonal_by_raster_values.py
+++ zonal_by_raster_values.py
@@ -3,6 +3,7 @@ import argparse
 import collections
 import logging
 import os
+import shutil
 import tempfile
 
 from ecoshard import geoprocessing
@@ -37,8 +38,7 @@ if __name__ == '__main__':
         help='interpolation mode for sampling, default "near"')
     args = parser.parse_args()
 
-    target_zonal_table = f'''zonal_{args.value_raster_path}_{
-        _base_filename(args.zone_raster_path)}.csv'''
+    target_zonal_table = f'''zonal_{args.value_raster_path}.csv'''
 
     working_dir = tempfile.mkdtemp(
         dir='.', prefix=_base_filename(target_zonal_table))
@@ -49,21 +49,21 @@ if __name__ == '__main__':
         _base_filename(args.zone_raster_path)}''')
     clipped_value_raster_path = os.path.join(
         working_dir, os.path.basename(args.value_raster_path))
-    geoprocessing.align_and_resize_raster_stack(
-        [args.value_raster_path], [clipped_value_raster_path],
-        [args.interpolation_mode], raster_info['pixel_size'],
-        raster_info['bounding_box'],
+    geoprocessing.warp_raster(
+        args.value_raster_path, raster_info['pixel_size'],
+        clipped_value_raster_path, args.interpolation_mode,
+        target_bb=raster_info['bounding_box'],
         target_projection_wkt=raster_info['projection_wkt'])
 
-    stats_dict = collections.defaultdict({
+    stats_dict = collections.defaultdict(lambda: {
         'min': numpy.iinfo(int).max,
         'max': numpy.iinfo(int).min,
-        'count': 0,
-        'sum': 0
+        'count': 0.0,
+        'sum': 0.0
     })
 
     offset_blocks = list(geoprocessing.iterblocks(
-        args.value_raster_path, offset_only=True))
+        (clipped_value_raster_path, 1), offset_only=True))
 
     zone_raster = gdal.OpenEx(args.zone_raster_path, gdal.OF_RASTER)
     zone_band = zone_raster.GetRasterBand(1)
@@ -75,32 +75,33 @@ if __name__ == '__main__':
 
     LOGGER.info(f'collecting stats for {len(offset_blocks)} raster blocks')
     for offset_block in offset_blocks:
-        value_array = clipped_value_band.ReadAsArray(**offset_blocks)
-        zone_array = zone_band.ReadAsArray(**offset_blocks)
+        value_array = clipped_value_band.ReadAsArray(**offset_block)
+        zone_array = zone_band.ReadAsArray(**offset_block)
         valid_mask = ~numpy.isclose(value_array, value_nodata) & (
             zone_array != zone_nodata)
         valid_value = value_array[valid_mask]
         valid_zone = zone_array[valid_mask]
         for zone_val in numpy.unique(zone_array[valid_mask]):
+            zone_mask = valid_zone == zone_val
             stats_dict[zone_val]['min'] = min(
-                stats_dict[zone_val]['min'], valid_value)
+                stats_dict[zone_val]['min'], numpy.min(valid_value[zone_mask]))
             stats_dict[zone_val]['max'] = max(
-                stats_dict[zone_val]['max'], valid_value)
-            stats_dict[zone_val]['count'] += valid_value.size
-            stats_dict[zone_val]['sum'] += numpy.sum(valid_value)
+                stats_dict[zone_val]['max'], numpy.max(valid_value[zone_mask]))
+            stats_dict[zone_val]['count'] += numpy.count_nonzero(zone_mask)
+            stats_dict[zone_val]['sum'] += numpy.sum(valid_value[zone_mask])
 
     LOGGER.info(f'writing stats to {target_zonal_table}')
     zone_list = sorted(stats_dict)
     csv_table_dict = collections.defaultdict(list)
     for zone_id in zone_list:
         csv_table_dict['zone'].append(zone_id)
-        csv_table_dict['min'].append(stats_dict[zone_val]['min'])
-        csv_table_dict['max'].append(stats_dict[zone_val]['max'])
-        csv_table_dict['count'].append(stats_dict[zone_val]['count'])
-        csv_table_dict['sum'].append(stats_dict[zone_val]['sum'])
-        if stats_dict[zone_val]['count'] > 0:
+        csv_table_dict['min'].append(stats_dict[zone_id]['min'])
+        csv_table_dict['max'].append(stats_dict[zone_id]['max'])
+        csv_table_dict['count'].append(stats_dict[zone_id]['count'])
+        csv_table_dict['sum'].append(stats_dict[zone_id]['sum'])
+        if stats_dict[zone_id]['count'] > 0:
             csv_table_dict['mean'].append(
-                stats_dict[zone_val]['sum']/stats_dict[zone_val]['count'])
+                stats_dict[zone_id]['sum']/stats_dict[zone_id]['count'])
         else:
             csv_table_dict['mean'].append(0)
 
@@ -108,3 +109,4 @@ if __name__ == '__main__':
     df = pandas.DataFrame.from_dict(csv_table_dict)
     column_names = ['zone', 'min', 'max', 'mean', 'count', 'sum']
     df.to_csv(target_zonal_table, columns=column_names, index=False)
+    shutil.rmtree(working_dir)
